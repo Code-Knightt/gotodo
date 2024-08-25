3560b3459a19a80ea5f7c3c577d0cca1e9bf8dd7
Code-knightt <bhagyapatel50125@gmail.com>
2024-08-25 17:44:50 +0530

    Project Setup

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gotodo",
+	Short: "Todo List in the command line built with GO",
+	Long: `Create, manage, complete and delete your tasks
+right from the command line. Powered by GO and cobra-cli
+	`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Code-knightt/gotodo/cmd"
+
+func main() {
+	cmd.Execute()
+}
